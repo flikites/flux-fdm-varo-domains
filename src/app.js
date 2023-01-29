86f0798757b6948fc65402a76deb9aba614ba9bc
motailab <mdmotailab@gmail.com>
2023-01-29 08:59:54 +0600

    changed way to fetching zones records.

--- src/app.js
+++ src/app.js
@@ -123,6 +123,9 @@ async function deleteRecord(ip, domain_name, zone_name) {
 }
 
 async function getRecords(domain_name, zone_name) {
+  domain_name = domain_name.includes(".")
+    ? domain_name.split(".")[1]
+    : domain_name;
   const url = `${DNS_SERVER_ADDRESS}/api/zones/records/get?token=${DNS_SERVER_TOKEN}&domain=${domain_name}&zone=${zone_name}`;
   console.log("url ", url);
   const { data } = await axios.get(url);
