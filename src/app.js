5f1d889d39cdbf373bbb38fa8939862c96cde7b8
motailab <mdmotailab@gmail.com>
2023-05-04 12:27:25 +0600

    added single ip support.

--- src/app.js
+++ src/app.js
@@ -9,174 +9,198 @@ const {
   api,
 } = require("./utils");
 
-async function checkIP({ app_name, app_port, domain_name }) {
+async function checkIP({ app_name, app_port, domain_names }) {
   try {
-    // Select 5 random URLs
-    const randomFluxNodes = await getWorkingNodes();
-
-    const randomUrls = randomFluxNodes.map(
-      (ip) => `http://${ip}:16127/apps/location/${app_name}`
-    );
-
-    const requests = randomUrls.map((url) =>
-      axios.get(url).catch((error) => {
-        console.log(`Error while making request to ${url}: ${error}`);
-      })
-    );
-
-    const responses = await axios.all(requests).catch((error) => {
-      console.log(`Error while making concurrent requests: ${error}`);
-    });
-
-    let responseData = [];
-    for (let i = 0; i < responses.length; i++) {
-      if (responses[i] && responses[i].data) {
-        const data = responses[i].data.data;
-        responseData.push(data.map((item) => item.ip));
-      }
-    }
-
-    // Find the most common IP
-    const commonIps = findMostCommonResponse(responseData).map((ip) => {
-      if (ip.includes(":")) {
-        return ip.split(":")[0];
-      }
-      return ip;
-    });
-
-    const { records, zone } = await getZoneAndRecords(domain_name, app_port);
-    console.log("app_name: ", app_name);
-    console.log("app_port: ", app_port);
-    console.log("flux Consensus Ip list for app: ", commonIps);
-    console.log("dns server returned records: ", records);
-
-    for (const ip of commonIps) {
-      try {
-        await createOrDeleteRecord(ip, records, app_port, domain_name, zone);
-      } catch (error) {
-        console.log(error?.message ?? error);
-      }
+    console.log(domain_names);
+    const randomUrls = await getRandomUrls(app_name);
+    const responses = await makeConcurrentRequests(randomUrls);
+    const responseData = getResponseData(responses);
+    const commonIps = findCommonIps(responseData);
+    console.log("commonIps ", commonIps);
+    const healthyIps = await findHealthyIps(commonIps, app_port);
+    console.log("healthyIps ", healthyIps);
+    if (healthyIps?.length) {
+      const { records, zone } = await getZoneAndRecords(
+        app_name,
+        app_port,
+        healthyIps
+      );
+      console.log("records ", records);
+      await processDomainNames(domain_names, healthyIps, records, zone);
+    } else {
+      console.log("there is no healthy ips so application is quiting.");
     }
   } catch (error) {
     console.error(error?.message ?? error);
   }
 }
 
-async function createOrDeleteRecord(
-  selectedIp,
-  records = [],
-  app_port,
-  domain_name,
-  zone_name
-) {
-  // Check if the selected IP returns success response
-  const connected = await checkConnection(selectedIp, app_port);
-  const { data: r2 } = await axios.get(
-    `https://api.incolumitas.com/?q=${selectedIp}`
+async function getRandomUrls(app_name) {
+  const randomFluxNodes = await getWorkingNodes();
+  return randomFluxNodes.map(
+    (ip) => `http://${ip}:16127/apps/location/${app_name}`
   );
-  let isGood = true;
-  if (
-    r2?.is_datacenter ||
-    r2?.is_tor ||
-    r2?.is_proxy ||
-    r2?.is_vpn ||
-    r2?.is_abuser
-  ) {
-    isGood = false;
-    console.log("bad user ip detected: ", selectedIp);
-  }
+}
 
-  const record = records.find(
-    (r) => r.content === selectedIp && r.name === domain_name
+async function makeConcurrentRequests(randomUrls) {
+  const requests = randomUrls.map((url) =>
+    axios.get(url).catch((error) => {
+      console.log(`Error while making request to ${url}: ${error}`);
+    })
   );
-  if (connected && isGood) {
-    if (!record) {
-      console.log(
-        `Creating new record for IP: ${selectedIp} in VARO DNS Server`
-      );
-      // Create new DNS record
-      await api.post("", {
-        action: "addRecord",
-        zone: zone_name,
-        type: "A",
-        name: domain_name,
-        content: selectedIp,
-      });
-    } else {
-      console.log(
-        `Record for IP: ${selectedIp} already exists in VARO DNS Server`
-      );
+  return await axios.all(requests).catch((error) => {
+    console.log(`Error while making concurrent requests: ${error}`);
+  });
+}
+
+function getResponseData(responses) {
+  let responseData = [];
+  for (let i = 0; i < responses.length; i++) {
+    if (responses[i] && responses[i].data) {
+      const data = responses[i].data.data;
+      responseData.push(data.map((item) => item.ip));
     }
-  } else if ((!connected || !isGood) && record) {
-    console.log(`Unsuccessful response from IP: ${selectedIp}`);
-    await api
-      .post("", {
-        action: "deleteRecord",
-        zone: zone_name,
-        record: record.uuid,
-      })
-      .catch((e) => console.log(e));
-    console.log(`IP: ${selectedIp} deleted `);
+  }
+  return responseData;
+}
+
+function findCommonIps(responseData) {
+  return findMostCommonResponse(responseData).map((ip) => {
+    if (ip.includes(":")) {
+      return ip.split(":")[0];
+    }
+    return ip;
+  });
+}
+
+async function findHealthyIps(commonIps, app_port) {
+  const healthyIps = [];
+  for (const ip of commonIps) {
+    try {
+      await checkConnection(ip, app_port);
+      healthyIps.push(ip);
+    } catch (error) {
+      console.log("flux returned a bad ip we are excluding from commonIps", ip);
+    }
+  }
+  return healthyIps;
+}
+
+async function processDomainNames(domain_names, healthyIps, records, zone) {
+  for (const [index, domainName] of domain_names.entries()) {
+    const ip = index < healthyIps.length ? healthyIps[index] : healthyIps[0];
+    try {
+      await createOrDeleteRecord(ip, records, domainName, zone);
+    } catch (error) {
+      console.log(error?.message ?? error);
+    }
   }
 }
 
-async function getZoneAndRecords(name, port) {
+async function createOrDeleteRecord(
+  selectedIp,
+  records = [],
+  domain_name,
+  zone_name
+) {
+  const record = records.find((r) => r.name === domain_name);
+
+  if (!record && selectedIp && domain_name) {
+    console.log(
+      `Creating new record for IP: ${selectedIp} for name ${domain_name} in VARO DNS Server`
+    );
+    // Create new DNS record
+    const { data } = await api.post("", {
+      action: "addRecord",
+      zone: zone_name,
+      type: "A",
+      name: domain_name,
+      content: selectedIp,
+    });
+    console.log("d ", data);
+  } else {
+    console.log(
+      `Record for IP: ${selectedIp} already exists in VARO DNS Server`
+    );
+  }
+}
+
+async function getZoneAndRecords(name, port, commonIps) {
   let zone = "";
   let records = [];
   console.log("processing dns zone for name: ", name);
-  try {
-    const { data } = await api.post("", {
-      action: "getZones",
-    });
-    let domain = name;
-    if (domain.includes(".")) {
-      const split = domain.split(".");
-      domain = `${split[split.length - 2]}.${split[split.length - 1]}`;
-    }
-    console.log("data ", data);
-    const z = data.data.find((z) => z.name === domain);
-    if (!z) {
-      const { data } = await api.post("", {
-        action: "createZone",
-        domain: domain,
-      });
 
-      zone = data.data.zone;
-      console.log(`zone created ${zone} NAME: ${domain}`);
+  try {
+    const { data } = await api.post("", { action: "getZones" });
+    // const domain = getDomainFromName(name);
+    const existingZone = data.data.find((z) => z.name === name);
+
+    if (existingZone) {
+      console.log(`zone exists ${existingZone.name}:${existingZone.id}`);
+      zone = existingZone.id;
     } else {
-      console.log(`zone exist ${z.name}:${z.id}`);
-      zone = z.id;
+      const { data: createdZone } = await api.post("", {
+        action: "createZone",
+        name,
+      });
+      zone = createdZone.data.zone;
+      console.log(`zone created ${zone} NAME: ${name}`);
     }
 
     const { data: recordsData } = await api.post("", {
       action: "getRecords",
-      zone: zone,
+      zone,
     });
-    records = [];
+
     for (const record of recordsData.data ?? []) {
       try {
         await checkConnection(record.content, port);
         records.push(record);
       } catch (error) {
-        console.log(error);
-        console.log("deleting ip from dns server ", record.content);
-        await api
-          .post("", {
-            action: "deleteRecord",
-            zone: zone,
-            record: record.uuid,
-          })
-          .catch((e) => console.log(e?.message));
+        console.log(
+          `detected a bad record with name: ${record.name} and ip ${record.content}`
+        );
+
+        const newIp =
+          findHealthyNewIp(commonIps, data.result) ?? getRandomIp(commonIps);
+        console.log(
+          `replacing bad ip: ${record.content} with new ip:${newIp} for domain:${record.name}`
+        );
+
+        await api.post("", {
+          action: "updateRecord",
+          zone,
+          record: record.id,
+          column: "content",
+          value: newIp,
+        });
+
+        record.content = newIp;
+        records.push(record);
       }
     }
-    return { records, zone };
   } catch (error) {
     console.log(
       "Unable to get or create zone or get DNS records: ",
       error?.message
     );
-    return { records, zone };
   }
+  return { records, zone };
+}
+
+// function getDomainFromName(name) {
+//   if (!name.includes(".")) return name;
+//   const split = name.split(".");
+//   return `${split[split.length - 2]}.${split[split.length - 1]}`;
+// }
+
+function findHealthyNewIp(commonIps, records) {
+  return commonIps.find((ip) => !records.find((r) => r.content === ip));
+}
+
+function getRandomIp(commonIps) {
+  const randomIndex = Math.floor(Math.random() * commonIps.length);
+  return commonIps[randomIndex];
 }
 
 // checkIP();
--- src/main.js
+++ src/main.js
@@ -3,27 +3,21 @@ dotenv.config();
 const cron = require("node-cron");
 const { checkIP } = require("./app");
 
-const APPS_NAME = process.env.APP_NAME?.split(",");
-const APPS_PORT = process.env.APP_PORT?.split(",");
+const APP_NAME = process.env.APP_NAME?.trim();
+const APP_PORT = process.env.APP_PORT.trim();
 const DOMAINS_NAME = process.env.DOMAIN_NAME?.split(",");
 
 async function main() {
-  let workers = [];
-  for (let i = 0; i < APPS_NAME.length; i++) {
-    workers.push(
-      checkIP({
-        app_name: APPS_NAME[i],
-        app_port: APPS_PORT[i],
-        domain_name: DOMAINS_NAME[i],
-      })
-    );
-  }
-  await Promise.all(workers);
+  checkIP({
+    app_name: APP_NAME,
+    app_port: APP_PORT,
+    domain_names: DOMAINS_NAME,
+  });
 }
 
 if (require.main === module) {
   main();
-  cron.schedule("*/15 * * * *", () => {
+  cron.schedule("*/1 * * * *", () => {
     main();
   });
 }
